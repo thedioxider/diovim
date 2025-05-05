ed66b35eea8e601580bdccf797c6934b67d02166
Demetrius <thedioxider@mail.ru>
2025-05-05 20:31:29 +0300

    feat(colorscheme): some colorschemes. currently use evergarden

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,23 @@
 return {
   { "cseelus/vim-colors-lucid" },
+  { "xfyuan/nightforest.nvim" },
+  { "mathofprimes/nightvision-nvim" },
+  { "everviolet/nvim" },
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_enable_italic = true
+      vim.g.everforest_better_performance = true
+      vim.g.everforest_background = 'hard'
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "lucid",
+      colorscheme = "evergarden",
     },
   }
 }
