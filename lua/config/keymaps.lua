67f8c7bd0f26cee72a95765f3874fed6a0e1a5aa
Demetrius <thedioxider@mail.ru>
2025-06-22 14:08:59 +0300

    feat(far): search & replace in buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n",
+  "<leader>sR",
+  "<cmd>lua require('grug-far').open({ prefills = { paths = vim.fn.expand('%') } })<cr>",
+  { desc = "Search and Replace (buffer)" }
+)
