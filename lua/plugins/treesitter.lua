ac4ed69d71024ee9264f6e883896db6288ff7bae
Demetrius <thedioxider@mail.ru>
2026-03-31 04:37:47 +0300

    style: apply stylua formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,18 +5,18 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    treesitter = {
-      highlight = true, -- enable/disable treesitter based highlighting
-      indent = true, -- enable/disable treesitter based indentation
-      auto_install = true, -- enable/disable automatic installation of detected languages
-      ensure_installed = {
-        "lua",
-        "vim",
-        -- add more arguments for adding more treesitter parsers
-      },
-    },
-  },
+	"AstroNvim/astrocore",
+	---@type AstroCoreOpts
+	opts = {
+		treesitter = {
+			highlight = true, -- enable/disable treesitter based highlighting
+			indent = true, -- enable/disable treesitter based indentation
+			auto_install = true, -- enable/disable automatic installation of detected languages
+			ensure_installed = {
+				"lua",
+				"vim",
+				-- add more arguments for adding more treesitter parsers
+			},
+		},
+	},
 }
